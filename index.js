0b44c53ceb7b9343b6fa005af05b29e85134a830
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2025-01-08 23:11:43 +0000

    npm

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const fs = require("fs")
+// const fs = require("fs")
 
 // fs.writeFile("message.txt", "Hello Jose", (err) => {
 //     if (err) throw err
 //     console.log('The file has been saved')
 // })
 
-fs.readFile("./test.js", "utf8", (err, data) => {
-    if(err) throw err
-    console.log(data)
-})
+// fs.readFile("./test.js", "utf8", (err, data) => {
+//     if(err) throw err
+//     console.log(data)
+// })
+
+import generateName from "sillyname"
+
+const sillyName = generateName();
+
+console.log(`My name is ${sillyName}`)
