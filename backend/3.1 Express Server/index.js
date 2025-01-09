c6b494d99f944a0b5046282dd3cb2ba553d3eb88
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2025-01-09 16:49:25 +0000

    HTTP

--- backend/3.1 Express Server/index.js
+++ backend/3.1 Express Server/index.js
@@ -4,7 +4,11 @@ const app = express()
 const port = 3000
 
 app.get("/", (req, res) => {
-    res.send("<h1> Hello there</h1>")
+    res.send("<h1> Hello Jose</h1>")
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1> Hello whats up</h1><p>My name is Jose Parra</p>")
 })
 
 app.listen(port, () => {
