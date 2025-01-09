af0d3823d026da87eda3f50b3d2719f616c9655a
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2025-01-09 15:34:22 +0000

    HTTP request GET

--- backend/3.1 Express Server/index.js
+++ backend/3.1 Express Server/index.js
@@ -3,6 +3,11 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+app.get("/", (req, res) => {
+    res.send("<h1> Hello there</h1>")
+})
+
 app.listen(port, () => {
     console.log(`Server running in port ${port}`)
 })
+
